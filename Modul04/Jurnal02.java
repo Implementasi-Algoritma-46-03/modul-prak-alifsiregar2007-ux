1632cc5d6e1a03eefbee5cd2d9c3dc36bce472dd
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-10-09 18:41:22 +0700

    Modul02

--- Modul04/Jurnal02.java
+++ Modul04/Jurnal02.java
@@ -9,19 +9,19 @@ public class Jurnal02 {
         double pajak = 0;
         if (penghasilan <= 50){
             pajak = penghasilan * 10000000 * 0.05;
-            System.out.println("Penghasilankotor = " + penghasilan + " juta Rupiah");
+            System.out.println("Penghasilan kotor = " + penghasilan + " juta Rupiah");
             System.out.println("Pajak 5% = Rp. " + (int) pajak);
             System.out.println("Penghasilan bersih = Rp. " + (int) (penghasilan * 10000000 - pajak));   
         }
         else if (penghasilan <= 500){
             pajak = penghasilan * 10000000 * 0.15;
-            System.out.println("Penghasilankotor = " + penghasilan + " juta Rupiah");
+            System.out.println("Penghasilan kotor = " + penghasilan + " juta Rupiah");                                      
             System.out.println("Pajak 5% = Rp. " + (int)pajak);
             System.out.println("Penghasilan bersih = Rp. " + (int) (penghasilan * 10000000 - pajak));
         }
         else {
             pajak = penghasilan * 10000000 * 0.30;
-            System.out.println("Penghasilankotor = " + penghasilan + " juta Rupiah");
+            System.out.println("Penghasilan kotor = " + penghasilan + " juta Rupiah");
             System.out.println("Pajak 5% = Rp. " + (int) pajak);
             System.out.println("Penghasilan bersih = Rp. " + (int) (penghasilan * 10000000 - pajak));
         }
