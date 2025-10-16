b525c10a1fadb17a21f0fc81bdea8a7873011e00
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-10-16 17:21:36 +0700

    Jurnal1-2-3

--- Modul05/Jurnal02.java
+++ Modul05/Jurnal02.java
@@ -1,6 +1,50 @@
-public class Jurnal02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
-    }
+public class Jurnal02 {
+   public Jurnal02() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      String var2 = var1.next().toLowerCase();
+      int var3 = var1.nextInt();
+      String[] var4 = new String[]{"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"};
+      boolean var5 = true;
+      byte var8;
+      switch (var2) {
+         case "senin":
+            var8 = 0;
+            break;
+         case "selasa":
+            var8 = 1;
+            break;
+         case "rabu":
+            var8 = 2;
+            break;
+         case "kamis":
+            var8 = 3;
+            break;
+         case "jumat":
+            var8 = 4;
+            break;
+         case "sabtu":
+            var8 = 5;
+            break;
+         case "minggu":
+            var8 = 6;
+            break;
+         default:
+            System.out.println("Hari tidak valid!");
+            return;
+      }
+
+      int var6 = (var8 + var3) % 7;
+      System.out.println(capitalize(var4[var6]));
+      var1.close();
+   }
+
+   static String capitalize(String var0) {
+      String var10000 = var0.substring(0, 1).toUpperCase();
+      return var10000 + var0.substring(1);
+   }
 }
--- Modul05/Jurnal03.java
+++ Modul05/Jurnal03.java
@@ -1,6 +1,36 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();       // bilangan pertama
+        char O = scanner.next().charAt(0); // tanda operasi
+        int B = scanner.nextInt();       // bilangan kedua
+
+     switch (O) {
+            case '+':
+                System.out.println(A + B);
+                break;
+            case '-':
+                System.out.println(A - B);
+                break;
+            case '*':
+                System.out.println(A * B);
+                break;
+            case '/':
+                if (B == 0) {
+                    System.out.println("Error");
+                } else {
+                    double hasil = (double) A / B;
+                    // cetak dengan 7 angka di belakang koma (dibulatkan)
+                    System.out.printf("%.7f%n", hasil);
+                }
+                break;
+            default:
+                System.out.println("Operator tidak valid");
+        }
+
+        scanner.close();
     }
 }
