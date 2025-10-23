e1a74a50c593e6102229e3b00994ce066356619f
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-10-23 16:44:49 +0700

    TP1-2-3

--- Modul06/Jurnal01.java
+++ Modul06/Jurnal01.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner s = new Scanner(System.in);
+        String nama1 = s.nextLine();
+        String nama2 = s.nextLine();
+        int waktu = s.nextInt();
+
+        System.out.println("Halo, " + nama1 + ". " + waktuSapa(waktu));
+        System.out.println("Halo, " + nama2 + ". " + waktuSapa(waktu));
+    
+    }
+    private static String waktuSapa(int waktu) {
+        if (waktu >= 06 && waktu <= 11)
+            return "Selamat pagi.";
+        else if (waktu >= 12 && waktu <= 14)
+            return "Selamat siang.";
+        else if (waktu >= 15 && waktu <= 17)
+            return "Selamat sore.";
+        else
+            return "Selamat malam.";
     }
 }
