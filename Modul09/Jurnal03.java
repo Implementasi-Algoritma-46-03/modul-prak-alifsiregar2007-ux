4fbf3ec2dffbfc6b0454b44b6370aa058c48d186
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-11-13 16:44:01 +0700

    upt

--- Modul09/Jurnal03.java
+++ Modul09/Jurnal03.java
@@ -1,20 +1,23 @@
 import java.util.*;
 public class Jurnal03 {
-    public static void main(String[] args) {
+
+    public static void main(final String[] args) {
+        // Kerjakan soalnya di sini
         Scanner input = new Scanner(System.in);
         int angka = input.nextInt();
 
         int angkaa = 0;
         String hasil = null;
-        for (int i=1; i <= angkaa; i++) {
+        for (int i= 1; i <= angka; i++) {
             if (angka % i == 0)
             angkaa++;
         }
+
         if (angkaa == 2)
             hasil = "YA";
         else 
             hasil = "BUKAN";
-    
+
         System.out.println(hasil);
     }
-} 
+}
