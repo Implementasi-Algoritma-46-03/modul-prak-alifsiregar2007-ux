7579c50fecb01f483965efe3a626332921784cb1
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-11-20 16:39:06 +0700

    push

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -4,7 +4,7 @@ public class Jurnal01 {
         // Kerjakan soalnya di sini 
         for (int i = 1; i <= 6; i++) {
             if (i % 2 == 0) {
-                System.out.print(" ");
+                System.out.print(" " + i);
             }
             for (int j = 1; j <= 5; j++) {
                 System.out.print(i + " ");
