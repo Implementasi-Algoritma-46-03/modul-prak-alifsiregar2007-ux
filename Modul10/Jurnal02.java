3a3efa4830103a60b5f16d57fb27f89238deaa48
alifsiregar2007-ux <alifsiregar2007@gmail.com>
2025-11-20 16:17:22 +0700

    jurnal

--- Modul10/Jurnal02.java
+++ Modul10/Jurnal02.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        int bilanganBulat = input.nextInt();
+
+        for (int i = 1; i <= bilanganBulat; i++) {
+            for (int j = 1; j <= bilanganBulat; j++) {
+                int hasilPerkalian = j * i;
+                System.out.printf("%-6d", hasilPerkalian);
+            }
+
+            System.out.println();
+        }
+
+        input.close();
     }
 }
--- Modul10/Jurnal03.java
+++ Modul10/Jurnal03.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Jurnal03 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int bilanganSaatIni = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - (i - 1); j++) {
+                System.out.print(bilanganSaatIni + " ");
+                bilanganSaatIni = (bilanganSaatIni + 1) % 10;
+            }
+
+            System.out.println();
+        }
+
+        input.close();
     }
 }
