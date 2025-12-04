031985f5db86b8ed5d0db1747c288355f1d97140
alifsiregar2007-ux <alifsiregar2007@ghmail.com>
2025-12-04 15:49:47 +0700

    pushhhhhhh

--- Modul12/Jurnal03.java
+++ Modul12/Jurnal03.java
@@ -1,6 +1,42 @@
-public class Jurnal03 {
+ public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        int mulai = 0;
+        int counter = 0;
+        int akhir = N;
+        int loop = 0;
+        
+        int[][] matriks = new int[N][N];
+
+        while (akhir > 0) {
+            for (int i = mulai; i<akhir; i++) {
+                counter++;
+                matriks[i + loop][mulai+loop] = counter;
+            }
+            akhir--;
+            for (int i= mulai; i<akhir; i++) {
+                counter++;
+                matriks[akhir + loop][i+ 1+ loop] = counter;
+            }
+            for (int i = mulai; i<akhir; i++) {
+                counter++;
+                matriks[akhir - i - 1 + loop][akhir + loop] = counter;
+            }
+            akhir--;
+            for (int i = mulai; i<akhir; i++) {
+                counter++;
+                matriks[mulai+loop][akhir - i + loop] = counter;
+            }
+            loop++;
+        }        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print(matriks[i][j]);
+                if (j < N - 1)
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
